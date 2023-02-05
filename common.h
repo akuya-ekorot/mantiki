9e3dcd7ed479e8b7be9133c62b11e48349d04aa5
Akuya Ekorot <akuyaekorot@gmail.com>
2023-02-05 13:37:04 +0300

    delete files

--- common.h
+++
@@ -1,8 +0,0 @@
-#ifndef mantiki_common_h
-#define mantiki_common_h
-
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-
-#endif /* mantiki_common_h */
--- debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef mantiki_debug_h
-#define mantiki_debuh_h
-
-#include "chunk.h"
-
-void disassemble_chunk(Chunk *chunk, const char *name);
-int disassemble_instruction(Chunk *chunk, int offset);
-
-#endif /* mantiki_debug_h */
